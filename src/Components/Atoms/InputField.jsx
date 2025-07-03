146dbbe933efd7145f26a0a03aa781dbf2d8837b
SafaSELLAM <safa.sellam@outlook.fr>
2025-07-03 14:34:58 +0200

    ADD inputfield atom

--- src/Components/Atoms/InputField.jsx
+++ src/Components/Atoms/InputField.jsx
@@ -1,8 +1,8 @@
-export const InputField = ({ label, id, value, onChange, type = "text" }) => {
+export const InputField = ({ label, id, value, onChange, type = "text", required = "true" }) => {
     return (
         <>
             <label htmlFor={id}>{label}</label>
-            <input id={id} type={type} value={value} onChange={onChange} />
+            <input id={id} type={type} value={value} onChange={onChange} required={required} />
         </>
     )
 }
