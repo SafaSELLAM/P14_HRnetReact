9a2a4507f39094431f042e7fd757d1871d191fa0
SafaSELLAM <safa.sellam@outlook.fr>
2025-07-03 14:40:10 +0200

    Add redux to manage datas

--- src/Store/EmployeeSlice.js
+++ src/Store/EmployeeSlice.js
@@ -1,7 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { generateMockEmployees } from "./mockEmployeeData.js"
+
+
+const initializeEmployees = () => {
+    return generateMockEmployees(500)
+}
 
 const initialState = {
-    employees: []
+    employees: initializeEmployees()
 }
 
 const employeeSlice = createSlice({
@@ -10,9 +16,17 @@ const employeeSlice = createSlice({
     reducers: {
         addEmployee: (state, action) => {
             state.employees.push(action.payload)
-        }
+        },
+        resetWithMockData: (state, action) => {
+            const count = action.payload || 500
+            const mockData = generateMockEmployees(count)
+            state.employees = mockData
+        },
+        clearAllEmployees: (state) => {
+            state.employees = []
+        },
     }
 })
 
-export const { addEmployee } = employeeSlice.actions
+export const { addEmployee, resetWithMockData, clearAllEmployees } = employeeSlice.actions
 export default employeeSlice.reducer
